d8efb811998e66f6f910b3e41384ac2bc62804d1
Mark Cooper <markchristophercooper@gmail.com>
2018-02-23 17:33:49 -0800

    Add reset

--- authorizer.sh
+++ authorizer.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+### RESET
+echo "Deleting AAT records"
+for file in ./data/auth/aat/*.xml; do rm "$file"; done
+echo "Deleting DTS records"
+for file in ./data/auth/dts/*.xml; do rm "$file"; done
+echo "Deleting LOC records"
+for file in ./data/auth/loc/*.xml; do rm "$file"; done
+
+echo "Deleting logs and sql"
+> authorizer.log
+rm -f authorizer.csv authorizer.sql
+
+echo "Running database migrations"
 bundle exec sequel -m db/migrations -M 0 'sqlite://db/authorizer.db'
 bundle exec sequel -m db/migrations 'sqlite://db/authorizer.db'
 bundle exec rake authorizer:db:populate_from_file
-
